a4db070327735201438cfb41523087fc5cbdd904
Teogenes <teogenes.santos@gmail.com>
2020-04-04 13:50:48 -0300

    Criacao e implementacao da Classe Account

---
+++ src/entities/Account.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Account {
+	private Integer number;
+	private String holder;
+	private Double balance, withDrawLimit;
+	
+	public Account() {
+		
+	}
+	public Account(Integer number, String holder, Double balance, Double withDrawLimit) {
+		super();
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withDrawLimit = withDrawLimit;
+	}
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	public String getHolder() {
+		return holder;
+	}
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	public Double getWithDrawLimit() {
+		return withDrawLimit;
+	}
+	public void setWithDrawLimit(Double withDrawLimit) {
+		this.withDrawLimit = withDrawLimit;
+	}
+	
+	public void deposit(Double amount) {
+		
+	}
+	public void withDraw(Double amount) {
+		
+	}
+	
+}
